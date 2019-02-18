231507c2038d505576b5170ad140abed5668b05f
Sergey Semyonov <semenov@restream.rt.ru>
2019-02-19 00:16:27 +0400

    Fix formatting in src/clj/scramblies_challenge/handler.clj

--- src/clj/scramblies_challenge/handler.clj
+++ src/clj/scramblies_challenge/handler.clj
@@ -1,9 +1,9 @@
 (ns scramblies-challenge.handler
   (:require
-   [reitit.ring :as reitit-ring]
-   [scramblies-challenge.middleware :refer [middleware]]
-   [hiccup.page :refer [include-js include-css html5]]
-   [config.core :refer [env]]))
+    [reitit.ring :as reitit-ring]
+    [scramblies-challenge.middleware :refer [middleware]]
+    [hiccup.page :refer [include-js include-css html5]]
+    [config.core :refer [env]]))
 
 (def mount-target
   [:div#app
@@ -20,10 +20,10 @@
 
 (defn loading-page []
   (html5
-   (head)
-   [:body {:class "body-container"}
-    mount-target
-    (include-js "/js/app.js")]))
+    (head)
+    [:body {:class "body-container"}
+     mount-target
+     (include-js "/js/app.js")]))
 
 (defn index-handler
   [_request]
@@ -33,14 +33,14 @@
 
 (def app
   (reitit-ring/ring-handler
-   (reitit-ring/router
-    [["/" {:get {:handler index-handler}}]
-     ["/items"
-      ["" {:get {:handler index-handler}}]
-      ["/:item-id" {:get {:handler index-handler
-                          :parameters {:path {:item-id int?}}}}]]
-     ["/about" {:get {:handler index-handler}}]])
-   (reitit-ring/routes
-    (reitit-ring/create-resource-handler {:path "/" :root "/public"})
-    (reitit-ring/create-default-handler))
-   {:middleware middleware}))
+    (reitit-ring/router
+      [["/" {:get {:handler index-handler}}]
+       ["/items"
+        ["" {:get {:handler index-handler}}]
+        ["/:item-id" {:get {:handler index-handler
+                            :parameters {:path {:item-id int?}}}}]]
+       ["/about" {:get {:handler index-handler}}]])
+    (reitit-ring/routes
+      (reitit-ring/create-resource-handler {:path "/" :root "/public"})
+      (reitit-ring/create-default-handler))
+    {:middleware middleware}))
